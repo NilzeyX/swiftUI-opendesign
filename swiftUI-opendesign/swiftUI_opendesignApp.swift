b2238a5bd8bbcecd9e418f4510b321880ebc625c
nilzeyX <nilesboyeswork@gmail.com>
2025-03-01 11:54:32 -0400

    Initial Commit

---
+++ swiftUI-opendesign/swiftUI_opendesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_opendesignApp.swift
+//  swiftUI-opendesign
+//
+//  Created by Niles Boyes on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_opendesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUI-opendesignTests/swiftUI_opendesignTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_opendesignTests.swift
+//  swiftUI-opendesignTests
+//
+//  Created by Niles Boyes on 3/1/25.
+//
+
+import Testing
+@testable import swiftUI_opendesign
+
+struct swiftUI_opendesignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
