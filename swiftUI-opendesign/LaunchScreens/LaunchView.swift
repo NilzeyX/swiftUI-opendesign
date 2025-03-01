3692f58640a52823d55e8318aba2e1c6272fe69a
nilzeyX <nilesboyeswork@gmail.com>
2025-03-01 13:59:46 -0400

    Used LaunchViewModel to create a simple loading screen with Animation.
    Will present everything through contentView

--- swiftUI-opendesign/LaunchScreens/LaunchView.swift
+++ swiftUI-opendesign/LaunchScreens/LaunchView.swift
@@ -7,22 +7,28 @@
 import SwiftUI
 
 struct LaunchView: View {
+    @ObservedObject private var viewModel = LaunchViewModel()
+
     var body: some View {
         ZStack {
             LinearGradient.noisyPurpleTeal()
                 .edgesIgnoringSafeArea(.all)
 
-            VStack (spacing: 20){
-                Image(systemName: "ev.plug.ac.gb.t.fill")
+            VStack(spacing: 20) {
+                Image(systemName: "circles.hexagonpath.fill")
                     .resizable()
-                    .rotationEffect(.degrees(180))
                     .scaledToFit()
-                    .frame(width: UIScreen.main.bounds.width * 0.4) // 40% of screen width
+                    .frame(width: UIScreen.main.bounds.width * 0.3) // 30% of screen width
                     .foregroundStyle(.white)
-                
+                    .shadow(radius: 2, x: 0, y: 0)
+                    .rotationEffect(.degrees(viewModel.isLoading ? 360 : 0))
+                    .animation(.linear(duration: 2).repeatForever(autoreverses: false), value: viewModel.isLoading)
+                    .onAppear { viewModel.startLoading() }
+
                 Text("Noisy Gradient with simple Reactive Elements")
                     .font(.system(size: UIScreen.main.bounds.width * 0.05).bold()) // 5% of screen width
                     .multilineTextAlignment(.center)
+                    .shadow(radius: 5, x: 0, y: 1)
             }
         }
     }
